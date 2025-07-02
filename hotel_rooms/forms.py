3fd151b66fb4f631bc7cc4a83d0010b6ce4ae783
akashsharma2706 <akashsharma2276@gmail.com>
2025-07-02 16:46:56 +0530

    updates

--- hotel_rooms/forms.py
+++ hotel_rooms/forms.py
@@ -13,7 +13,7 @@ class RoomForm(forms.ModelForm):
     class Meta:
         model = Room
         fields = ['room_number', 'room_type', 'room_image', 'price', 'is_booked']
-        
+
 
 class ReviewForm(forms.ModelForm):
     class Meta:
